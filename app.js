a025e8929ffa7bce462f4df151f54b7915807eff
Najma Saeed <najmasy20@gmail.com>
2023-09-13 21:05:52 +0100

    app.js

---
+++ app.js
@@ -0,0 +1,27 @@
+//Requirements
+//Add at least 3 dynamic components to your web page. These can be based on user input, or other dynamic input data.
+
+//At least one dynamic component should be based on user input.
+
+//At least one dynamic component should use a conditional statement to determine the output.
+
+//Stretch Goals
+//Try out the confirm() function, in addition to prompt(). When’s the right time to use this?
+
+//If the user gives you a color, can you set a part of your page to be that color?
+
+
+//user input
+//prompt()
+
+//try confirm()
+//user gives a colour, set a part of your page to that colour
+
+//conditional statement to determine the output
+
+
+let yourName = prompt("What is your name?");
+
+console.log(yourName);
+
+
